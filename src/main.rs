d3ca962977c2b05424c7c3efd3d20844a4641bff
Leon Sering <leon@sering.eu>
2021-05-12 18:07:54 +0200

    First implementation of a metric via NxN-array (using const generics)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-struct cluster_problem{
+struct ClusterProblem{
     // metric space
     // number of colors (gamma)
     // number of points n
@@ -11,12 +11,44 @@ struct cluster_problem{
     // Solving algorithm as method? Maybe not.
 }
 
-struct space{
-    // distance matrix (later maybe implicit as distance function to avoid n^2 space)
-    // points as array or implicit? If implicity: array of color-classes.
+
+// Trait to obtain the distance between two points.
+pub trait Metric{
+    fn dist(&self, x1 : usize, x2 : usize) -> f32; // returns the distance between x1 and x2
 }
 
-struct clustering{
+
+// Trait to obtain the color class of a point.
+pub trait Colored{
+    fn color(&self, x : usize) -> u8;
+}
+
+// a metric space of N point with color classes. Distances are given by NxN-array and color class
+// by N-array.
+pub struct Space<const N: usize>{
+    distances: [[f32; N]; N], // distance matrix (later maybe implicit as distance function to avoid n^2 space)
+    colors: [u8; N], // points as array or implicit? If implicity: array of color-classes.
+}
+
+/*pub fn new_space(distances : [[f32; N]; N], colors: [u8; N]) -> Space<const N> {
+    Space<const N> {
+        distances,
+        colors,
+    }
+}*/
+impl<const N: usize> Metric for Space<N> {
+    fn dist(&self, x1: usize, x2: usize) -> f32 {
+        self.distances[x1][x2]
+    }
+}
+
+impl<const N: usize> Colored for Space<N> {
+    fn color(&self, x: usize) -> u8 {
+        self.colors[x]
+    }
+}
+
+struct Clustering{
     // array of centers / center indices
     // radius
     // assignment of points to centers
@@ -25,6 +57,18 @@ struct clustering{
 
 fn main() {
 
+    // create test metric space with 3 points
+
+    const N : usize = 3;
+    let space1 : Space<{N}> = Space {
+        distances : [[0.0, 1.0, 2.0],
+                     [1.0, 0.0, 3.0],
+                     [2.0, 3.0, 0.0]],
+        colors : [0, 0, 1],
+    };
+
+    println!("dist between 1 and 2: {}", space1.dist(1,2));
+    println!("color of 2: {}", space1.color(2));
     // phase 1: Gonzalez heuristic
     // Output: S[0] to S[k]
     
@@ -33,9 +77,6 @@ fn main() {
     // phase 3: modify assignment, s.t. sizes are multiple of L
     
     // phase 4: open new centers and  determine actual set of centers C
-
-
-
 
 
 }
