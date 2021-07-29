a1f5a41f07cece9e32a54f8aae8d0fa45c29427a
Leon Sering <leon@sering.eu>
2021-07-29 09:13:05 +0200

    Added delete mode into the python script: create_2d_space.py

--- python_scripts/create_2d_space.py
+++ python_scripts/create_2d_space.py
@@ -7,30 +7,54 @@ from matplotlib import colors
 import tkinter as tk
 from tkinter import filedialog
 
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.set_xlim([-10, 10])
 ax.set_ylim([-10, 10])
 
+class point:
+    def __init__(self, x, y, c, mpl_point):
+        self.x = x
+        self.y = y
+        self.c = c
+        self.mpl_point = mpl_point
 
-x = []
-y = []
-c = []
+points = []
 
 colormap = ['k','r','g','c','m','y','b',"orange","lime","pink"]
 def onclick(event):
     x_value = round(event.xdata,2)
     y_value = round(event.ydata,2)
-    x.append(x_value)
-    y.append(y_value)
-    c.append(current_color)
-    print("plot point", (x_value, y_value), "with color:", current_color)
-    plt.scatter(x_value, y_value, c = colormap[current_color])
+
+    if current_color == 'delete mode':
+        delete_threshold = 1
+        current_dist_squared = delete_threshold + 1
+        for p in points:
+            dist_squared = (x_value - p.x)**2 + (y_value - p.y)**2
+            if dist_squared < current_dist_squared:
+                current_dist_squared = dist_squared
+                current_point = p
+        if current_dist_squared < delete_threshold:
+            current_point.mpl_point.remove()
+            points.remove(current_point)
+            print("removed point", (current_point.x, current_point.y, current_point.c), "dist_squared:", current_dist_squared)
+        else:
+            print("no point removed.")
+    else:
+        mpl_point = plt.scatter(x_value, y_value, c = colormap[current_color])
+
+        points.append(point(x_value, y_value, current_color,mpl_point))
+        print("plot point", (x_value, y_value), "with color:", current_color)
+
     fig.canvas.draw()
 
 def on_press(event):
     global current_color
     key = event.key
+    if key == 'delete':
+        current_color = 'delete mode'
+        print("current color changed to:", current_color)
     if key.isdigit():
         current_color = int(key)
         print("current color changed to:", current_color)
@@ -43,12 +67,16 @@ def on_close(event):
     file_path = file.name
     print(file_path)
     with open(file_path, 'a') as writer:
-        for i in range(len(x)):
-            writer.write(f"{x[i]},{y[i]},{c[i]}\n")
+        for i in range(len(points)):
+            writer.write(f"{points[i].x},{points[i].y},{points[i].c}\n")
 
 
 global current_color 
 current_color = 0
+
+print("""Use digit-keys (0 to 9) to set colors. (default: 0). Click on the canvas to create a new point. 
+Use 'del' to go into delete mode: Click nearby a point to delete it. Exit delete mode by choosing a color.
+2dspace can be written to a file after closing the windows.""")
 
 fig.canvas.mpl_connect('key_press_event', on_press)
 fig.canvas.mpl_connect('button_press_event', onclick)
