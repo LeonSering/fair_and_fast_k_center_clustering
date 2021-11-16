68da0bcae553b717e109392a79eec4a137a8ff6c
Leon Sering <leon@sering.eu>
2021-11-16 17:25:44 +0100

    Bug can finally be reproduced. Somehow the rerun of phase 2 makes problems.

--- src/main.rs
+++ src/main.rs
@@ -26,13 +26,13 @@ fn main() {
         // rep_intervals : vec!((0,2),(1,2),(2,6),(3,3),(0,40),(0,500)), // representation interval [a,b] for each color class; for color classes without interval we subsitute [0. inf]
     // };
 
-    let verbose = 1; // 0: silent, 1:brief, 2:verbose
+    let verbose = 2; // 0: silent, 1:brief, 2:verbose
 
     let space_name = "maritalCreditCard";
     let space = SpaceND::by_file(format!("{}.2dspace",space_name).as_str(),15,verbose);
     let prob = ClusteringProblem{
         k : 21, // number of center;
-        privacy_bound : 56, // number of points to represent;
+        privacy_bound : 48, // number of points to represent;
         rep_intervals : vec!((7,500);3), // representation interval [a,b] for each color class; for color classes without interval we subsitute [0. inf]
     };
 
