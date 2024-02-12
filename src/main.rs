e187078f83170e306e0eb8292bb088d6f28802ee
Leon Sering <leon@sering.eu>
2024-02-12 16:40:30 +0100

    renamed to from ff_k-center to ff_k_center

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,17 @@ fn main() {
     let n = 5_000;
     let k = 8;
     let space = SpaceND::new_random(n);
-    let prob = ClusteringProblem{
-        k, // number of center;
-        privacy_bound : n/k, // number of points to represent;
-        rep_intervals : vec!((0,500),(0,500),(0,500)), // representation interval [a,b] for each color class; for color classes without interval we subsitute [0. inf]
+    let prob = ClusteringProblem {
+        k,                                                 // number of center;
+        privacy_bound: n / k,                              // number of points to represent;
+        rep_intervals: vec![(0, 500), (0, 500), (0, 500)], // representation interval [a,b] for each color class; for color classes without interval we subsitute [0. inf]
     };
 
-    let (clustering, total_time) = compute_privacy_preserving_representative_k_center(&space, &prob, None);
-    println!("Radius: {}; Running time: {}s.", clustering.get_radius(), total_time);
+    let (clustering, total_time) =
+        compute_privacy_preserving_representative_k_center(&space, &prob, None);
+    println!(
+        "Radius: {}; Running time: {}s.",
+        clustering.get_radius(),
+        total_time
+    );
 }
